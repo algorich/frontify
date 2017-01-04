b8ff098b2e3bcb2c8d069bc4850f4aa67b5c77b5
Dyogo Ribeiro Veiga <dyogo.nsi@gmail.com>
2017-01-04 09:49:08 -0200

    Add assets and routes configuration

--- lib/generators/frontify/setup_generator.rb
+++ lib/generators/frontify/setup_generator.rb
@@ -4,6 +4,12 @@ module Frontify
 
     def create_alg_components_configuration
       copy_file 'frontify.yml', "config/frontify.yml"
+      inject_into_file 'config/routes.rb', after: "Rails.application.routes.draw do\n" do
+        "  mount Frontify::Engine => '/frontify'\n"
+      end
+      inject_into_file 'config/initializers/assets.rb', before: "Rails.application.config.assets.version" do
+        "Rails.application.config.assets.paths << 'vendor/frontify/'\n"
+      end
     end
   end
 end
